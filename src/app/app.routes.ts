f7da5dadf531ceb19cee69bac1089c2637366a65
ahmad-hawchar <hawchar.business@gmail.com>
2023-11-25 17:50:14 +0200

    All folders are created, code is not written yet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { ListComponent } from './ListPage/pages/list/list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: ListComponent },
+    { path: "authentication", component: LoginComponent },
+    { path: "**", component: PageNotFoundComponent },
+];
