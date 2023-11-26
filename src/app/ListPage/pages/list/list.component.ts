46d5420e506fa7cb5d1dcfc697dbe07bc5d167d9
ahmad-hawchar <hawchar.business@gmail.com>
2023-11-27 00:07:54 +0200

    adjusting styles

--- src/app/ListPage/pages/list/list.component.ts
+++ src/app/ListPage/pages/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -9,10 +10,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-  notes: { title: string, content: string, color: string }[] = [
-    { title: "test", content: "test", color: "test" },
-    { title: "test", content: "test", color: "test" },
-    { title: "test", content: "test", color: "test" },
-    { title: "test", content: "test", color: "test" }
+  constructor(private router: Router) { }
+  notes: { title: string, content: string, color: string, date: string }[] = [
+    { title: "test", content: "test", color: "red", date: Date().substring(0, 10) },
+    { title: "test", content: "test", color: "cyan", date: Date().substring(0, 10) },
+    { title: "test", content: "test", color: "orange", date: Date().substring(0, 10) },
+    { title: "test", content: "test", color: "pink", date: Date().substring(0, 10) },
+    { title: "test", content: "test", color: "green", date: Date().substring(0, 10) },
+    { title: "test", content: "test", color: "orange", date: Date().substring(0, 10) },
+    { title: "test", content: "test", color: "red", date: Date().substring(0, 10) }
   ]
+  logout() {
+    localStorage.clear();
+    this.router.navigateByUrl("/authentication")
+  }
 }
